26fc6eeb039d06c06ea8cbaafc7d34c6ea0ff8d0
K\Kento <KTC24A31A0043@edu.kyoto-tech.ac.jp>
2026-03-02 21:12:42 +0900

    ConstantBufferのCPP途中

--- directx/ConstantBuffer.cpp
+++ directx/ConstantBuffer.cpp
@@ -1,1 +1,20 @@
 #include "ConstantBuffer.h"
+#include<cassert>
+
+
+namespace {
+	const auto heapType_ = D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV;
+}
+
+ConstantBuffer::~ConstantBuffer() {
+	DescriptorHeapContainer::instance().releaseDescriptor(heapType_, descriptorIndex_);
+}
+
+bool ConstantBuffer::create(UINT bufferSize)noexcept {
+	const auto size = (sizeof(bufferSize) + 255) & ~255;
+
+	D3D12_HEAP_PROPERTIES heapProps{};
+	heapProps.Type = D3D12_HEAP_TYPE_UPLOAD;
+	D3D12_RESOURCE_DESC resorcesDesc{};
+    
+}
